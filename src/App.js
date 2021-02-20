1f787bf32d842d4c27ce63e81303b07821db47c2
Codekacode <karem.quiroz.m@gmail.com>
2021-02-20 01:00:47 -0500

    Create card recipes and add stile to input and button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react"; 
+import Recipie from './Recipie'
 import './App.css';
 
 const App = () => {
@@ -6,24 +7,46 @@ const App = () => {
   const APP_KEY = "1888f37e9bf20b7c4496f1201eb4f73d";
 
   const [recipies, setRecipies] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
 
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipies(data.hits);
+    console.log(data.hits);
 
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
   }
 
   return (
     <div className="app">
-      <form action="" className="search__form">
-        <input type="text" className="search__bar"/>
+      <form onSubmit={getSearch} action="" className="search__form">
+        <input type="text" className="search__bar" value={search} onChange={updateSearch} />
         <button type="submit" className="search__btn">Search</button>
       </form>
+      {recipies.map(recipie => (
+        <Recipie 
+          key={recipie.recipe.label}
+          title={recipie.recipe.label} 
+          calories={recipie.recipe.calories} 
+          image={recipie.recipe.image}
+          ingredients={recipie.recipe.ingredients}
+        />
+      ))}
     </div>
   );
 };
