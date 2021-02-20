8a49df5d43aefad10e7e42141ab30e0952e15735
Codekacode <karem.quiroz.m@gmail.com>
2021-02-19 23:13:09 -0500

    Acces to recipes API and testo into app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react"; 
 import './App.css';
 
-function App() {
+const App = () => {
+  const APP_ID = "156c89d6";
+  const APP_KEY = "1888f37e9bf20b7c4496f1201eb4f73d";
+
+  const [recipies, setRecipies] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipies(data.hits);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form action="" className="search__form">
+        <input type="text" className="search__bar"/>
+        <button type="submit" className="search__btn">Search</button>
+      </form>
     </div>
   );
-}
+};
+
 
 export default App;
